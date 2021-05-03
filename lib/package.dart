3646ae032845e6f8d4e8931aab936d3d893a4b25
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-03 16:10:25 +0700

    commit

---
+++ lib/package.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+part 'package:pielove_food_order/pages/home.dart';
---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+part of 'package:pielove_food_order/package.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
