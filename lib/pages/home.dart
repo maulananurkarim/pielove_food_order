dc03e68b042e1bba0af3e3ef0d5333b0f9e02958
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-04 10:27:49 +0700

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,9 +10,50 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: 460,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/PICT.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 30,
+              left: 30,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      "assets/BACK_BUTTON.png",
+                      height: 14,
+                      width: 8,
+                    ),
+                    Text(
+                      "Pie Details",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Image.asset(
+                      "assets/LOVE_BUTTON.png",
+                      height: 18,
+                      width: 20,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
